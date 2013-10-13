8957ad9bfb0a944ec6e6328819d0b1bc1988a853
MortenPlaypen <morten@playpenlabs.com>
2013-10-13 09:46:58 -0700

    Added Hilepicker.io

--- onlineqrreader.js
+++ onlineqrreader.js
@@ -1,13 +1,36 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+
+  Template.upload.greeting = function () {
     return "Welcome to onlineqrreader.";
   };
 
-  Template.hello.events({
+  Template.upload.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+        console.log("You pressed the upload button");
+        var key = "AVBBdW4z9R3ybh8LG1XPcz";
+        console.log(key);
+        filepicker.setKey("key");
+        filepicker.pick(function(InkBlob){
+          console.log(InkBlob.url);
+          Meteor.call('sendEmail', 'lundsby@gmail.com','lundsby@gmail.com','QR Code Uploaded',InkBlob.url);
+        });
+    }
+  });
+
+  Template.take_picture.greeting = function () {
+  return "Welcome to onlineqrreader.";
+  };
+
+  Template.take_picture.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the take picture button");
     }
   });
 }
@@ -16,4 +39,21 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  Meteor.methods({
+  sendEmail: function (to, from, subject, text) {
+    check([to, from, subject, text], [String]);
+
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  }
+});
 }
+
