38c1bb8e7d0fd07c90e5396d1471b97bfa1602fd
MortenPlaypen <morten@playpenlabs.com>
2013-10-13 13:23:24 -0700

    Added tracking

--- onlineqrreader.js
+++ onlineqrreader.js
@@ -1,6 +1,7 @@
 if (Meteor.isClient) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    var analytics=analytics||[];(function(){var e=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page","pageview","ab","alias","ready","group"],t=function(e){return function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var n=0;n<e.length;n++)analytics[e[n]]=t(e[n])})(),analytics.load=function(e){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)};
+    analytics.load("xdhpak9s5e");
   });
 
   Template.upload.greeting = function () {
@@ -17,6 +18,7 @@ if (Meteor.isClient) {
         filepicker.pick(function(InkBlob){
           console.log(InkBlob.url);
           Meteor.call('sendEmail', 'lundsby@gmail.com','lundsby@gmail.com','QR Code Uploaded',InkBlob.url);
+          analytics.track('Upload and send');
         });
     }
   });
@@ -29,6 +31,7 @@ if (Meteor.isClient) {
     'click input' : function () {
       if (typeof console !== 'undefined')
         console.log("You pressed the take picture button");
+        analytics.track('Take pic and send');
     }
   });
 }
