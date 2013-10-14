cb009fc3c5fa029cfb64be1c90485521251958e9
MortenPlaypen <morten@playpenlabs.com>
2013-10-14 00:32:56 -0700

    Made views dynamic

--- onlineqrreader.js
+++ onlineqrreader.js
@@ -13,18 +13,20 @@ if (Meteor.isClient) {
     var analytics=analytics||[];(function(){var e=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page","pageview","ab","alias","ready","group"],t=function(e){return function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var n=0;n<e.length;n++)analytics[e[n]]=t(e[n])})(),analytics.load=function(e){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)};
     analytics.load("xdhpak9s5e");
     key = "AVBBdW4z9R3ybh8LG1XPcz"; // Filepicker token
+    Session.set("currentPage", "no_file");
   });
 
   Template.upload.events({
     'click input' : function () {
       if (typeof console !== 'undefined')
-        console.log("You pressed the upload button");
+        console.log("You pressed the upload button"); //TEST
         //filepicker.setKey("key");
         //filepicker.pick(function(Pic){
           //picURL = Pic.url;
           picURL = "https://www.filepicker.io/api/file/LxqFMcvTSCanIn1gIZZx"; //TEST - force file
-          console.log("picURL from upload is: " + picURL);
+          console.log("picURL from upload is: " + picURL); //TEST
           //analytics.track('Upload pic');
+          Session.set("currentPage", "a_file");
         //});
     }
   });
@@ -32,13 +34,14 @@ if (Meteor.isClient) {
   Template.take_picture.events({
     'click input' : function () {
       if (typeof console !== 'undefined')
-        console.log("You pressed the take picture button");
+        console.log("You pressed the take picture button"); //TEST
         //filepicker.setKey("key");
-       // filepicker.pick(function(Pic){
+        //filepicker.pick(function(Pic){
           //picURL = Pic.url;
           picURL = "https://www.filepicker.io/api/file/LxqFMcvTSCanIn1gIZZx"; //TEST - force file
-          console.log("picURL from take pic is: " + picURL);
+          console.log("picURL from take pic is: " + picURL); //TEST
           //analytics.track('Take pic');
+          Session.set("currentPage", "a_file");
         //});
       }
   });
@@ -48,14 +51,22 @@ if (Meteor.isClient) {
     'click #btnsendcode' : function () {
       if (typeof console !== 'undefined')
         emailAddress = $("input[type=text]").val();
-        //emailAddress = 'test@testmail.com'; //TEST - force email, to be read from form
         Meteor.call('translateQR', picURL, emailAddress); //calls translate function
-        console.log("You pressed the submit button");
-        console.log("Submit: " + picURL);
-        console.log("Email: " + emailAddress);
+        console.log("You pressed the submit button"); //TEST
+        console.log("Submit: " + picURL); //TEST
+        console.log("Email: " + emailAddress); //TEST
         //analytics.track('Submitted QR');
+        Session.set("currentPage", "sent_file");
     }
   });
+
+  Template.content.helpers({
+  currentPage: function (type) {
+    var thePage = Session.get("currentPage");
+    return thePage === type;
+  }
+  });
+
 }
 
 if (Meteor.isServer) {
